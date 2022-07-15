a492d2d65b0b026f0f70b35044b0832b36e4c8bd
Cristian Ordonez <cristianordonez@pop-os.localdomain>
2022-07-14 22:34:17 -0400

    completed deleting item from mealplan

--- client/src/components/mealplan-day/index.tsx
+++ client/src/components/mealplan-day/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {Dispatch, SetStateAction} from 'react';
 import {MealplanItem} from './MealplanItem';
-
+import { AlertColor } from '@mui/material';
 export type MealplanItemType = {
     id: number; 
     position: number;
@@ -16,10 +16,15 @@ export type MealplanItemType = {
 
 interface Props {
     mealplanItems: MealplanItemType[];
+    setOpenSnackbar: Dispatch<SetStateAction<boolean>>;
+    setAlertSeverity: Dispatch<SetStateAction<AlertColor | undefined>>
+    setAlertMessage: Dispatch<SetStateAction<string>>;
+    setMealPlanItems: Dispatch<SetStateAction<[]>>; 
+    currentDay: string;
 }
 
 //gets list of meal plan items, then renders one mealplanitem component per item
-export const MealplanDay = ({mealplanItems}: Props) => {
+export const MealplanDay = ({mealplanItems, setMealPlanItems, setOpenSnackbar, setAlertSeverity, setAlertMessage, currentDay}: Props) => {
     console.log('mealplanItems', mealplanItems)
     if (mealplanItems.length) {
     return (
@@ -30,9 +35,15 @@ export const MealplanDay = ({mealplanItems}: Props) => {
             slot={item.slot}
             type={item.type}
             id={item.value.id}
+            shoppingListId={item.id}
             imageType={item.value.imageType}
             servings={item.value.servings}
             title={item.value.title}
+            setOpenSnackbar={setOpenSnackbar}
+            setAlertSeverity={setAlertSeverity}
+            setAlertMessage={setAlertMessage}
+            setMealPlanItems={setMealPlanItems}
+            currentDay={currentDay}
         />
         )}
         </>
