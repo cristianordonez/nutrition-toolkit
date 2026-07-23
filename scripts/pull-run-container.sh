142bdfdb2834a5f0b38cbfa830d4291c1a04fa19
Cristian Ordonez <cristianordonezrd@gmail.com>
2026-07-22 20:02:02 -0400

    updated env var name to use container name

--- scripts/pull-run-container.sh
+++ scripts/pull-run-container.sh
@@ -8,9 +8,9 @@ echo "=============================="
 
 echo "DOCKER_IMAGE: ${DOCKER_IMAGE}"
 echo "PORT: ${PORT}"
-echo "IMAGE_NAME: ${IMAGE_NAME}"
+echo "CONTAINER_NAME: ${CONTAINER_NAME}"
 
 docker pull "${DOCKER_IMAGE}"
-docker stop "${IMAGE_NAME}" || true
-docker rm "${IMAGE_NAME}" || true
-docker run -d --env DATABASE_HOST=host.docker.internal --env-file ~/.env -p ${PORT}:8080 --name "${IMAGE_NAME}" 
+docker stop "${CONTAINER_NAME}" || true
+docker rm "${CONTAINER_NAME}" || true
+docker run -d --env DATABASE_HOST=host.docker.internal --env-file ~/.env -p ${PORT}:8080 --name "${CONTAINER_NAME}" 
